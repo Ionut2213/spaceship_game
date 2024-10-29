3cde688827c7d9aa9decef451ffecf7ab8924d8a
laote <ihagiu1212@gmail.com>
2024-10-29 12:35:42 +0200

    creating colors and update the display

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ import pygame
 
 WIDTH = 900
 HEIGHT = 500
-
-
-
-
-
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space war ships")
+WHITE = (255, 255, 255)
 
 
 
@@ -25,6 +22,8 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
+        WIN.fill(WHITE)
+        pygame.display.update()
     pygame.quit()
 
 
