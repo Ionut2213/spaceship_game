56e92c1d44376242c9b22b00b4d9d8fbe38bbe20
laote <ihagiu1212@gmail.com>
2024-10-30 12:45:14 +0200

    created 2 functions that handle the movement for both spaceships

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ pygame.display.set_caption("Space war ships")
 WHITE = (255, 255, 255)
 
 FPS = 60
+VEL = 5
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 44
 
 
@@ -25,16 +26,50 @@ RED_SPACESHIP_IMAGE = pygame.image.load(
 
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
-def draw_window():
+def draw_window(red, yellow):
     WIN.fill(WHITE)
-    WIN.blit(YELLOW_SPACESHIP, (300, 100))
-    WIN.blit(RED_SPACESHIP, (700, 100))
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
+
+
+def yellow_ship_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a]: # left
+        yellow.x -= VEL
+
+    if keys_pressed[pygame.K_d]: # right
+        yellow.x += VEL
+
+    if keys_pressed[pygame.K_w]: # UP
+        yellow.y -= VEL
+
+    if keys_pressed[pygame.K_s]: # Down
+        yellow.y += VEL
+
+
+
+def red_ship_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT]: # left
+        red.x -= VEL
+
+    if keys_pressed[pygame.K_RIGHT]: # right
+        red.x += VEL
+
+    if keys_pressed[pygame.K_UP]: # UP
+        red.y -= VEL
+
+    if keys_pressed[pygame.K_DOWN]: # Down
+        red.y += VEL
+
+
 
 
 
 
 def main():
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -42,7 +77,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
+
+
+        keys_pressed = pygame.key.get_pressed()
+        yellow_ship_handle_movement(keys_pressed, yellow)
+        red_ship_handle_movement(keys_pressed, red)
+        draw_window(red, yellow)
         
         
     pygame.quit()
