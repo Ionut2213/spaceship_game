eb47c1fdbba940bf11b0671b70e1cff31947c535
laote <ihagiu1212@gmail.com>
2024-10-30 11:39:05 +0200

    implemented the fps

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space war ships")
 WHITE = (255, 255, 255)
 
-
+FPS = 60
 
 
 def draw_window():
@@ -19,12 +19,15 @@ def draw_window():
 
 
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         draw_window()
+        
         
     pygame.quit()
 
